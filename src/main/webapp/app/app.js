933db736cf53d267db75704d70ee829e245b2380
Sergiy Illichevskyy <illichso@gmail.com>
2015-10-13 18:26:26 +0200

    update

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -8,16 +8,34 @@
     var gems = [
         {
             name: 'Dodecahedron',
-            price: 2.95,
+            price: 2,
             description: ' . . . ',
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            images: [
+                {
+                    full: "resources/full.jpg",
+                    thumb:'resourcesthumb.jpg'
+                }
+            ],
+            images1: [
+                "images/gem-02.gif",
+                "images/gem-05.gif",
+                "images/gem-09.gif"
+            ],
         },
         {
             name: 'Pentagonal Gem',
             price: 5.95,
             description: ' . . . ',
             canPurchase: true,
-            soldOut: true
+            soldOut: false,
+            images: [
+                {
+                    full: 'resources/full.jpg',
+                    thumb:'resources/thumb.jpg'
+                }
+            ]
+
         }];
 })();
