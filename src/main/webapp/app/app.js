00f56b83ee166a656f28424c1ed84305bab384c3
Sergiy Illichevskyy <illichso@gmail.com>
2015-10-13 21:24:14 +0200

    add new Controller

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -38,4 +38,15 @@
             ]
 
         }];
+
+    app.controller("PanelController", function(){
+        this.tab = 1;
+
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+    });
 })();
